0b0d4bca7ed4646dade788996b6e3a8118985395
fnatterdal <frenat@student.chalmers.se>
2022-04-29 16:22:51 +0200

    Moved chairs to floor level + increased performance by changing lighting

--- FlammanVR/Assets/FlipSeat.cs
+++ FlammanVR/Assets/FlipSeat.cs
@@ -10,9 +10,11 @@ public class FlipSeat : MonoBehaviour
     private bool seatFolded;
     private Vector3 startRotation = new Vector3(80, 0, 0);
     private Vector3 stopRotation = new Vector3(-80, 0, 0);
-    private Vector3 up = new Vector3(-0.092022419f,1.67871296f+0.5f,-0.806179047f+1.3f);
-    private Vector3 down = new Vector3(-0.092022419f,2.9000001f,-0.0700000003f);
+    private Vector3 up = new Vector3(-0.092022419f,1.67871296f,-0.806179047f);
+    private Vector3 down = new Vector3(-0.092022419f,2.9000001f+3f,-0.0700000003f+2f);
     private Vector3 foldedSeatOffset;
+    //private Vector3 up = new Vector3(-0.092022419f, 2.51206064f, -0.00858958811f);
+    //private Vector3 down = new Vector3(-0.092022419f, 1.67999995f, -0.467000008f);
 
     public void Start()
     {
@@ -40,7 +42,7 @@ public class FlipSeat : MonoBehaviour
     private void unfoldSeat()
     {
         seat.GetComponent<Transform>().Rotate(stopRotation);
-        transform.position = transform.position + foldedSeatOffset;
+        transform.localPosition = transform.localPosition + foldedSeatOffset;
         seat.GetComponent<TeleportationAnchor>().enabled = true;
         seatMeshChecker.enabled = true;
     }
@@ -48,7 +50,7 @@ public class FlipSeat : MonoBehaviour
     private void foldSeat()
     {
         seat.GetComponent<Transform>().Rotate(startRotation);
-        transform.position = transform.position - foldedSeatOffset;
+        transform.localPosition = transform.localPosition - foldedSeatOffset;
         seat.GetComponent<TeleportationAnchor>().enabled = false;
         seatMeshChecker.enabled = false;
     }
