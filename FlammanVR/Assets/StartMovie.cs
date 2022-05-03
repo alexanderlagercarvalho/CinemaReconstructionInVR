52cdbebee974b2cc693bd7eb7ec713c889f084a9
JonEmilsson <jon.emilsson0522@gmail.com>
2022-05-03 11:08:52 +0200

    Films synced now?
    Added new method to allow synced movies needs testing

--- FlammanVR/Assets/StartMovie.cs
+++ FlammanVR/Assets/StartMovie.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class StartMovie : MonoBehaviour
 {
+    private PhotonView photonView;
     public GameObject cinemaScreen;
     // Start is called before the first frame update
     void Start()
@@ -11,11 +14,16 @@ public class StartMovie : MonoBehaviour
         cinemaScreen = GameObject.Find("NewCinemaScreen");
     }
 
+    [PunRPC]
     public void PlayMovie()
     {
         cinemaScreen.GetComponent<WorldSpaceVideo>().StartMovie();
         cinemaScreen.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
         cinemaScreen.GetComponent<Renderer>().material.SetColor("White", Color.white);
+    }
+   public void PlayMovieRPC()
+    {
+        photonView.RPC("PlayMovie", RpcTarget.All);
     }
 
 }
