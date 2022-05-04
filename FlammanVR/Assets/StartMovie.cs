74da238c77034c4bdede53a0965dfa940ca5790c
JonEmilsson <jon.emilsson0522@gmail.com>
2022-05-04 11:26:43 +0200

    Small Change

--- FlammanVR/Assets/StartMovie.cs
+++ FlammanVR/Assets/StartMovie.cs
@@ -11,6 +11,7 @@ public class StartMovie : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        photonView = GetComponent<PhotonView>();
         cinemaScreen = GameObject.Find("NewCinemaScreen");
     }
 
