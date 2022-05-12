fc88b4c2a440e424e9504ca2e8419c568550fc7d
JonEE <jon.emilsson0522@gmail.com>
2022-05-12 12:12:58 +0200

    SinglePlayer and Exit methods added

---
+++ FlammanVR/Assets/Scripts/SinglePlayerandExit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SinglePlayerandExit : MonoBehaviour
+{
+
+    public void SinglePLayerScene()
+    {
+        SceneManager.LoadScene("SinglePlayerScene");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
